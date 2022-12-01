5f25a0d52696e810dfc1e4aec559a75ac079c7fe
李洋 <liyang19991126@126.com>
2022-12-01 19:06:56 +0800

    update

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+import Map from "views/Map.js";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: "tim-icons icon-chart-pie-36",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/map",
+    name: "Map",
+    rtlName: "خرائط",
+    icon: "tim-icons icon-pin",
+    component: Map,
+    layout: "/admin"
+  }
+];
+export default routes;
---
+++ src/views/Dashboard.js
@@ -0,0 +1,115 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+
+import Example from "variables/data";
+// reactstrap components
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  Label,
+  FormGroup,
+  Input,
+  Table,
+  Row,
+  Col,
+  UncontrolledTooltip
+} from "reactstrap";
+
+// core components
+import {
+  chartExample1,
+  chartExample2,
+  chartExample3,
+  chartExample4
+} from "variables/charts.js";
+function Dashboard(props) {
+  const [bigChartData, setbigChartData] = React.useState("data1");
+  const setBgChartData = (name) => {
+    setbigChartData(name);
+  };
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col xs="12">
+            <Card className="card-chart">
+              <CardHeader>
+                <Row>
+                  <Col className="text-left" sm="6">
+                    <h5 className="card-category">CodeGeeX下载总量</h5>
+                    <CardTitle tag="h2">4648</CardTitle>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample1[bigChartData]}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+        </Row>
+        <Row>
+          <Col xs="12">
+            <Card className="card-chart">
+              <CardHeader>
+                <Row>
+                  <Col className="text-left" sm="6">
+                    <h5 className="card-category">CodeGeeX调用总量</h5>
+                    <CardTitle tag="h2">2056521</CardTitle>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+              
+                    data={chartExample1["data2"]}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
