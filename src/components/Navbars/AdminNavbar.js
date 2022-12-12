b5bde2e27b4a4af7afaf3c0342371591222fe554
dingxuyang <dingxuyang>
2022-12-12 17:55:41 +0800

    Initial commit

---
+++ src/components/Navbars/AdminNavbar.js
@@ -0,0 +1,57 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+
+// reactstrap components
+import {
+  Navbar,
+} from "reactstrap";
+
+function AdminNavbar(props) {
+  const [collapseOpen, setcollapseOpen] = React.useState(false);
+  const [color, setcolor] = React.useState("navbar-transparent");
+  React.useEffect(() => {
+    window.addEventListener("resize", updateColor);
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      window.removeEventListener("resize", updateColor);
+    };
+  });
+  // function that adds color white/transparent to the navbar on resize (this is for the collapse)
+  const updateColor = () => {
+    if (window.innerWidth < 993 && collapseOpen) {
+      setcolor("bg-white");
+    } else {
+      setcolor("navbar-transparent");
+    }
+  };
+  // this function opens and closes the collapse on small devices
+  return (
+    <>
+      <Navbar className={classNames("navbar-absolute", color)} expand="lg">
+          <div style={{ color: "white",fontSize:"30px" }} >
+            天启大模型
+          </div>
+      </Navbar>
+    </>
+  );
+}
+
+export default AdminNavbar;
---
+++ src/views/Dashboard.js
@@ -0,0 +1,604 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { useState, useEffect} from 'react';
+// nodejs library that concatenates classes
+// react plugin used to create charts
+import { Line } from "react-chartjs-2";
+
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Label,
+  Form,
+  FormGroup,
+  Input,
+  Row,
+  Col,
+} from "reactstrap";
+
+// core components
+import {
+  chartExample4
+} from "variables/charts.js";
+import { data } from 'jquery';
+function Dashboard(props) {
+  const [startDate, setStartDate] = useState("2022-12-01");
+  const [endDate, setEndDate] = useState("2022-12-5");
+  let list=[]
+  let datagex2=[]
+  let datagex3=[]
+  let datagex=[]
+  let datagex4=[]
+  let chart1_2_options = {
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    tooltips: {
+      backgroundColor: "#f5f5f5",
+      titleFontColor: "#333",
+      bodyFontColor: "#666",
+      bodySpacing: 4,
+      xPadding: 12,
+      mode: "nearest",
+      intersect: 0,
+      position: "nearest"
+    },
+    responsive: true,
+    scales: {
+      yAxes: [
+        {
+          barPercentage: 1.6,
+          gridLines: {
+            drawBorder: false,
+            color: "rgba(29,140,248,0.0)",
+            zeroLineColor: "transparent"
+          },
+          ticks: {
+            suggestedMin: 60,
+            suggestedMax: 125,
+            padding: 20,
+            fontColor: "#9a9a9a"
+          }
+        }
+      ],
+      xAxes: [
+        {
+          barPercentage: 1.6,
+          gridLines: {
+            drawBorder: false,
+            color: "rgba(29,140,248,0.1)",
+            zeroLineColor: "transparent"
+          },
+          ticks: {
+            padding: 20,
+            fontColor: "#9a9a9a"
+          }
+        }
+      ]
+    }
+  };
+
+  let chartExample1 = {
+    data1: (canvas) => {
+      let ctx = canvas.getContext("2d");
+  
+      let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+  
+      gradientStroke.addColorStop(1, "rgba(29,140,248,0.2)");
+      gradientStroke.addColorStop(0.4, "rgba(29,140,248,0.0)");
+      gradientStroke.addColorStop(0, "rgba(29,140,248,0)"); //blue colors
+  
+      return  {
+        labels: list,
+        datasets: [
+          {
+            label: "累计下载量",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: "#1f8ef1",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#1f8ef1",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#1f8ef1",
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: datagex
+          },
+          {
+            label: "日新增安装量",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: "#1f8ef1",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#1f8ef1",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#1f8ef1",
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: datagex2
+          },
+          {
+            label: "日活跃度",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: "#1f8ef1",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#1f8ef1",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#1f8ef1",
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: datagex3
+          },
+          {
+            label: "日调用",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: "#1f8ef1",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#1f8ef1",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#1f8ef1",
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: datagex4
+          }
+        ]
+      };
+    },
+    data2: (canvas) => {
+      let ctx = canvas.getContext("2d");
+  
+      let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+  
+      gradientStroke.addColorStop(1, "rgba(29,140,248,0.2)");
+      gradientStroke.addColorStop(0.4, "rgba(29,140,248,0.0)");
+      gradientStroke.addColorStop(0, "rgba(29,140,248,0)"); //blue colors
+  
+      return {
+        labels: list,
+        datasets: [
+          {
+            label: "累计下载量",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: "#1f8ef1",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#1f8ef1",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#1f8ef1",
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: datagex
+          },
+          {
+            label: "累计下载量",
+            fill: true,
+            type: 'bar',
+            backgroundColor: gradientStroke,
+            hoverBackgroundColor: gradientStroke,
+            borderColor: "#d048b6",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            data: datagex
+          }
+        ]
+      };
+    },
+    data3: (canvas) => {
+      let ctx = canvas.getContext("2d");
+  
+      let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+  
+      gradientStroke.addColorStop(1, "rgba(29,140,248,0.2)");
+      gradientStroke.addColorStop(0.4, "rgba(29,140,248,0.0)");
+      gradientStroke.addColorStop(0, "rgba(29,140,248,0)"); //blue colors
+  
+      return {
+        labels: list,
+        datasets: [
+          {
+            label: "日新增安装量",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: "#1f8ef1",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#1f8ef1",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#1f8ef1",
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: datagex2
+          },
+          {
+            label: "日新增安装量",
+            fill: true,
+            type: 'bar',
+            backgroundColor: gradientStroke,
+            hoverBackgroundColor: gradientStroke,
+            borderColor: "#d048b6",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            data: datagex2
+          }
+        ]
+      };
+    },
+    data4: (canvas) => {
+      let ctx = canvas.getContext("2d");
+  
+      let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+  
+      gradientStroke.addColorStop(1, "rgba(29,140,248,0.2)");
+      gradientStroke.addColorStop(0.4, "rgba(29,140,248,0.0)");
+      gradientStroke.addColorStop(0, "rgba(29,140,248,0)"); //blue colors
+  
+      return {
+        labels: list,
+        datasets: [
+          {
+            label: "日活跃度",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: "#1f8ef1",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#1f8ef1",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#1f8ef1",
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: datagex3
+          },
+          {
+            label: "日活跃度",
+            fill: true,
+            type: 'bar',
+            backgroundColor: gradientStroke,
+            hoverBackgroundColor: gradientStroke,
+            borderColor: "#d048b6",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            data: datagex3
+          }
+        ]
+      };
+    },
+    data5: (canvas) => {
+      let ctx = canvas.getContext("2d");
+  
+      let gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
+  
+      gradientStroke.addColorStop(1, "rgba(29,140,248,0.2)");
+      gradientStroke.addColorStop(0.4, "rgba(29,140,248,0.0)");
+      gradientStroke.addColorStop(0, "rgba(29,140,248,0)"); //blue colors
+  
+      return {
+        labels: list,
+        datasets: [
+          {
+            label: "日调用",
+            fill: true,
+            backgroundColor: gradientStroke,
+            borderColor: "#1f8ef1",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBackgroundColor: "#1f8ef1",
+            pointBorderColor: "rgba(255,255,255,0)",
+            pointHoverBackgroundColor: "#1f8ef1",
+            pointBorderWidth: 20,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 15,
+            pointRadius: 4,
+            data: datagex4
+          },
+          {
+            label: "日调用",
+            fill: true,
+            type: 'bar',
+            backgroundColor: gradientStroke,
+            hoverBackgroundColor: gradientStroke,
+            borderColor: "#d048b6",
+            borderWidth: 2,
+            borderDash: [],
+            borderDashOffset: 0.0,
+            data: datagex4
+          }
+        ]
+      };
+    },
+    options: chart1_2_options
+  };
+  function dataSum(a,b){
+    list=[]
+    datagex=[]
+    datagex2=[]
+    datagex3=[]
+    datagex4=[]
+
+    let request = new XMLHttpRequest();
+    request.open('GET', "http://10.50.74.216:32621/tracking/visualData/selectDaily?startTime="+a+"&endTime="+b, false);  // 同步请求
+    request.send(null);
+
+    let obj={}
+    if (request.status === 200) {
+       obj = JSON.parse(request.responseText);
+      console.log(obj) 
+      // console.log(request.responseText);
+    }
+
+      console.log(Object.keys(obj.data).length) 
+
+    // };
+
+    // fetchData();
+         for(var i=0;i<Object.keys(obj.data).length;i++)
+      {
+        list.push(obj.data[i]["dateTime"])
+        datagex.push(obj.data[i]["installsNumber"])
+        datagex2.push(obj.data[i]["addInstallsNumber"])
+        datagex3.push(obj.data[i]["userDau"])
+        datagex4.push(obj.data[i]["requestNumber"])
+
+      }
+
+    console.log(list,datagex) 
+  return {
+    labels: list,
+    datasets: [
+      {
+        label: "累计下载量",
+        fill: true,
+        borderColor: "#1f8ef1",
+        borderWidth: 2,
+        borderDash: [],
+        borderDashOffset: 0.0,
+        pointBackgroundColor: "#1f8ef1",
+        pointBorderColor: "rgba(255,255,255,0)",
+        pointHoverBackgroundColor: "#1f8ef1",
+        pointBorderWidth: 20,
+        pointHoverRadius: 4,
+        pointHoverBorderWidth: 15,
+        pointRadius: 4,
+        data: datagex
+      },
+      {
+        label: "日新增安装量",
+        fill: true,
+        borderColor: "#d048b6",
+        borderWidth: 2,
+        borderDash: [],
+        borderDashOffset: 0.0,
+        pointBackgroundColor: "#d048b6",
+        pointBorderColor: "rgba(255,255,255,0)",
+        pointHoverBackgroundColor: "#d048b6",
+        pointBorderWidth: 20,
+        pointHoverRadius: 4,
+        pointHoverBorderWidth: 15,
+        pointRadius: 4,
+        data: datagex2
+      },
+      {
+        label: "日活跃度",
+        fill: true,
+        borderColor: "#00FF7F",
+        borderWidth: 2,
+        borderDash: [],
+        borderDashOffset: 0.0,
+        pointBackgroundColor: "#00FF7F",
+        pointBorderColor: "rgba(255,255,255,0)",
+        pointHoverBackgroundColor: "#00FF7F",
+        pointBorderWidth: 20,
+        pointHoverRadius: 4,
+        pointHoverBorderWidth: 15,
+        pointRadius: 4,
+        data: datagex3
+      },
+      {
+        label: "日调用",
+        fill: true,
+        borderColor: "#9932CC",
+        borderWidth: 2,
+        borderDash: [],
+        borderDashOffset: 0.0,
+        pointBackgroundColor: "#9932CC",
+        pointBorderColor: "rgba(255,255,255,0)",
+        pointHoverBackgroundColor: "#9932CC",
+        pointBorderWidth: 20,
+        pointHoverRadius: 4,
+        pointHoverBorderWidth: 15,
+        pointRadius: 4,
+        data: datagex4
+      }
+    ]
+   }
+  }
+  useEffect(() => {
+      dataSum(startDate,endDate)
+  }, []);
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col xs="12">
+            <Card className="card-chart">
+              <CardHeader>
+
+                <Row>
+                  <Col className="text-left" sm="6">
+                    <h5 className="card-category">CodeGeeX</h5>
+                    <CardTitle tag="h2">总面板</CardTitle>
+                    </Col>
+                    <Col className="text-right" sm="6">
+                    <Form className="text-right" inline>
+                      <FormGroup>
+                        <Label for="exampleEmail">start date</Label>
+                        <Input
+                          type="date"
+                          name="start date"
+                          onChange={(e) => { setStartDate(e.target.value) }}
+                        />
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="examplePassword">end datey</Label>
+                        <Input
+                          type="date"
+                          name="end date"
+                          onChange={(e) => { setEndDate(e.target.value) }}
+                        />
+                      </FormGroup>
+                  </Form>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={dataSum(startDate, endDate)}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+
+
+        </Row>
+        <Row>
+          <Col xs="12">
+            <Card className="card-chart">
+              <CardHeader>
+                <Row>
+                  <Col className="text-left" sm="6">
+                    <h5 className="card-category">CodeGeeX</h5>
+                    <CardTitle tag="h2">累计下载</CardTitle>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+              
+                    data={chartExample1["data2"]}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+        <Col lg="4">
+            <Card className="card-chart">
+              <CardHeader>
+                <h5 className="card-category">CodeGeeX</h5>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-delivery-fast text-primary" />{" "}
+                  日新增
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample1["data3"]}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="4">
+            <Card className="card-chart">
+              <CardHeader>
+                <h5 className="card-category">CodeGeeX</h5>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-send text-success" /> 日活
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample1["data4"]}
+                    options={chartExample4.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="4">
+            <Card className="card-chart">
+              <CardHeader>
+                <h5 className="card-category">CodeGeeX</h5>
+                <CardTitle tag="h3">
+                  <i className="tim-icons icon-send text-success" /> 日调用
+                </CardTitle>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample1["data5"]}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
